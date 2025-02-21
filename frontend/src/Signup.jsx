8d37c2eb6e1dfa3009a519e60f29c4fcf683a1f8
Aniket-op <akumar51_be23@thapar.edu>
2025-02-21 09:55:48 +0530

    first commit

---
+++ frontend/src/Signup.jsx
@@ -0,0 +1,93 @@
+import Button from '@mui/material/Button';
+import TextField from "@mui/material/TextField";
+import {Card, Typography} from "@mui/material";
+import {useState} from "react";
+
+function Signup() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [firstName, setfirstName] = useState("")
+    const [lastName, setlastName] = useState("")
+
+    return <div>
+            <div style={{
+                paddingTop: 150,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <Typography variant={"h6"}>
+                Welcome to Course Selling App. Sign up below
+                </Typography>
+            </div>
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <Card varint={"outlined"} style={{width: 400, padding: 20}}>
+                <TextField
+                    onChange={(evant11) => {
+                        let elemt = evant11.target;
+                        setfirstName(elemt.value);
+                    }}
+                    fullWidth={true}
+                    label="First Name"
+                    variant="outlined"
+                />
+                <br/><br/>
+                <TextField
+                    onChange={(e) => {
+                        setlastName(e.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Last Name"
+                    variant="outlined"
+                />
+                <br/><br/>
+                <TextField
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Email"
+                    variant="outlined"
+                />
+                <br/><br/>
+                <TextField
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Password"
+                    variant="outlined"
+                    type={"password"}
+                />
+                <br/><br/>
+                <Button
+                    size={"large"}
+                    variant="contained"
+                    onClick={() => {
+                        function callback2(data) {
+                            localStorage.setItem("token", data.token);
+                        }
+                        function callback1(res) {
+                            res.json().then(callback2)
+                        }
+                        
+                        fetch("http://localhost:3000/api/v1/user/signup", {
+                            method: "POST",
+                            body: JSON.stringify({
+                                username: email,
+                                password: password
+                            }),
+                            headers: {
+                                "Content-type": "application/json"
+                            }
+                        })
+                        .then(callback1)
+                    }}
+
+                > Signup</Button>
+            </Card>
+        </div>
+    </div>
+}
+
+export default Signup;
